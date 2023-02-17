90ceac82ba8d8ce65c6f639bbd60639d111f04e6
Nasief Khan <nasiefk14@gmail.com>
2023-02-17 13:23:02 +0200

    Function Type Annotations

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # React-TypeScript-Tut
+
+- Types are the ones defined by the user after a variable name using the :/semicolon
+- Inference or Type Indeference is the TS compiler reading the code and adding its own type without a user defining the type
---
+++ Section-26/Features/Annotations/functions.ts
@@ -0,0 +1,9 @@
+const add = (a : number, b : number) : number => {
+    return a + b;
+};
+// We add the number as a type of the inputs as well as adding the number as a type in the fucntion so that the function will always expect a number as a return value
+// Adding the number as the function type give us a chcek that makes sure we return a number
+// It will check whether the function returns the proper value type but not the logic within the function
+// When adding an anottation type we need to have a return type in the function body
+
+  
