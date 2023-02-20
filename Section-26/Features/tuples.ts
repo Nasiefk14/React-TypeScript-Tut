0451a1eb0e2e3dccef64a2bbf7799caca0287358
Nasief Khan <nasiefk14@gmail.com>
2023-02-20 16:31:35 +0200

    tuples

---
+++ Section-26/Features/tuples.ts
@@ -0,0 +1,14 @@
+const drink  = {
+    color: 'brown',
+    carbonated: true,
+    sugar: 40
+};
+
+type Drink = [string, boolean, number];
+// The type defined now in this tuple is simalr to an array but only takes in value paramters, Im still confused on when and why we would use this
+
+const pepsi: [string, boolean, number] = ['brown',true,40];
+// Can be redone using the tuple type
+
+const sprite: Drink = ['clear', true, 22]
+// This example above uses the tuple as a type\
