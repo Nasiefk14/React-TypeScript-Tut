2a1f41d369e9e18f8a2df8890bcb8ec0f2d93ae6
Nasief Khan <nasiefk14@gmail.com>
2023-02-20 16:13:02 +0200

    Arrays

---
+++ Section-26/Features/arrays.ts
@@ -0,0 +1,12 @@
+const carMakers: string[] = ['fords', 'toyota', 'vw'];
+const carArray : string[] = [];
+// We add array types wehn initialise it so we dont have the any type, unless we initialise it with data like in line 1
+
+const carsBymake = [
+    ['f150'],
+    ['corolla'],
+    ['polo']
+];
+const carsBymaker : string[][] = [];
+// WE have the same example as above where we need to inililise when there is no data inside but we have here an array of string arrays
+
