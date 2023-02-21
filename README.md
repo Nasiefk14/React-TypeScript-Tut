4105d5e2b711d7afc58a6c6d401c5023ee3dc28f
Nasief Khan <nasiefk14@gmail.com>
2023-02-21 15:05:57 +0200

    class inheritance

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 - Types are the ones defined by the user after a variable name using the :/semicolon
 - Inference or Type Indeference is the TS compiler reading the code and adding its own type without a user defining the type
+- Tuples are most oftenly never used in TS
--- Section-26/Features/classes.ts
+++ Section-26/Features/classes.ts
@@ -14,3 +14,13 @@ const vehicle = new Vehicle();
 vehicle.drive();
 vehicle.honk();
 // Since we created vehicle as a new Vehicle we have all the functions attached to Vehicle too it
+
+class Car extends Vehicle {
+    drive(): void {
+        console.log('Vroooo Paaaa');
+    }
+};
+// We add on the extends keyword which allows us to make a new class name Car but with the properties of Vehicle class already added
+
+const car = new Car();
+// Here we have a car of type Car which has all the Vehicle methods attached to it
