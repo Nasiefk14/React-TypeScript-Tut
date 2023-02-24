aa58f4cda9f6ac1763866480ae4eaa34db5f4afc
Nasief Khan <nasiefk14@gmail.com>
2023-02-24 16:05:38 +0200

    UseRef & added useEffect

--- Section-2/react-typescript/src/index.tsx
+++ Section-2/react-typescript/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import EventComponent from './events/EventComponent';
+import UserSearch from './refs/UserSearch';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(
 
 const App = () => {
   return(
-      <EventComponent/>
+      <UserSearch/>
   )
 }
 
--- Section-2/react-typescript/src/refs/UserSearch.tsx
+++ Section-2/react-typescript/src/refs/UserSearch.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 
 const users = [
   { name: "Nasief", age: 24 },
@@ -7,21 +7,33 @@ const users = [
 ];
 
 const UserSearch: React.FC = () => {
+  const inputRef = useRef<HTMLInputElement | null>(null);
   const [name, setName] = useState("");
-  const [user, setUser] = useState<{name: string, age: number} | undefined>()
-  
+  const [user, setUser] = useState<{ name: string; age: number } | undefined>();
+
+  useEffect(() => {
+    if(!inputRef.current){
+      return
+    }
+    inputRef.current.focus();
+  }, []);
+
   const onClick = () => {
     const foundUser = users.find((user) => {
-        return user.name === name
-  });
+      return user.name === name;
+    });
 
-  setUser(foundUser)
-};
+    setUser(foundUser);
+  };
 
   return (
     <div>
       <h1>User Search</h1>
-      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <input
+        ref={inputRef}
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
       <button onClick={onClick}>Find User</button>
       <div>
         {user && user.name}
