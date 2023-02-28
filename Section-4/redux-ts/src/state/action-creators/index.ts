ed6256bedd70870c52e6465e22d356fc7c2592e6
Nasief Khan <nasiefk14@gmail.com>
2023-02-28 18:55:53 +0200

    Testing Commits From Command Line

--- Section-4/redux-ts/src/state/action-creators/index.ts
+++ Section-4/redux-ts/src/state/action-creators/index.ts
@@ -9,20 +9,23 @@ export const searchRepositories = (term: string) => {
     });
 
     try {
-        const { data } = await axios.get('https://registry.npmjs.org/-/v1/search', {
-            params: {
-                text: term
-            }
-        });
+      const { data } = await axios.get(
+        "https://registry.npmjs.org/-/v1/search",
+        {
+          params: {
+            text: term,
+          },
+        }
+      );
 
-        const names = data.objects.map((result:any) => {
-            return result.package.name;
-        });
+      const names = data.objects.map((result: any) => {
+        return result.package.name;
+      });
 
-        dispatch({
-            type: ActionType.SEARCH_REPOSITORIES_SUCCESS,
-            payload: names
-        });
+      dispatch({
+        type: ActionType.SEARCH_REPOSITORIES_SUCCESS,
+        payload: names,
+      });
     } catch (err) {
       if (err instanceof Error) {
         dispatch({
